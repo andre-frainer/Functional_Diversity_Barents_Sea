bb5709670705c94f429130d7427a7bc9aef3d4e7
André Frainer <73888283+andre-frainer@users.noreply.github.com>
2021-07-15 16:10:41 +0200

    Update cwv.R

--- cwv.R
+++ cwv.R
@@ -1,40 +1,32 @@
-# Community-weighted variance
+# Script for calculating community-weighted variance (CWV) and community-weighted mean (CWM) for multiple sites
 
-# abund |2300 x 54|
-# trait |54 x 11|
-# cwm   |2300 x 11|
+# Requirements: 
+# a species abundance data table ('a') with sites (rows) and species (columns)
+# a trait value data table ('t') with species (rows) and traits (columns)
 
-# Weighted variance
+# Attention: make sure to verify that the species list in table 'a' is at the same 
+
+# Load
 library(Hmisc)
-# wtd.var
 
-CWV <- function(x, a){
-  m <- matrix(0, dim(a)[1], dim(x)[2])
-  v <- matrix(0, dim(a)[1], dim(x)[2])
+Func.variance <- function(t, a){
+  m <- matrix(0, dim(a)[1], dim(t)[2])
+  v <- matrix(0, dim(a)[1], dim(t)[2])
   sum.a <- apply(a, 1, sum)
   a <- a/sum.a
    for(i in 1:dim(a)[1]) {
-    for(j in 1:dim(x)[2]) {
-    m[i, j] <- weighted.mean(x[, j], a[i, ])
-    v[i, j] <- wtd.var(x[, j], weights = a[i, ], 
+    for(j in 1:dim(t)[2]) {
+    m[i, j] <- weighted.mean(t[, j], a[i, ])
+    v[i, j] <- wtd.var(t[, j], weights = a[i, ], 
                        method = "ML",  na.rm = TRUE, normwt = TRUE)
     }
    }
-  colnames(v) <- colnames(x)
+  colnames(v) <- colnames(t)
   rownames(v) <- rownames(a)
-  colnames(m) <- colnames(x)
+  colnames(m) <- colnames(t)
   rownames(m) <- rownames(a)
   return(list(m = m, v = v))
   return(list(v = v))
-}
-
-cw <- function(x, w){
-  a <- x %>% 
-    group_by(., year) %>% 
-    mutate(map(x, sum))
-  aa <- x/a
-  purrr::map(wtd.var(w, weights = aa, 
-              method = "ML",  na.rm = TRUE, normwt = TRUE))
 }
 
 
